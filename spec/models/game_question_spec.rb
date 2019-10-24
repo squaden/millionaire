4889344dadeffb1d9ae648eee1bd20ae4c11f730
squaden <druzh.ds@gmail.com>
2019-10-25 00:32:19 +0300

    question, game_question and game tests

--- spec/models/game_question_spec.rb
+++ spec/models/game_question_spec.rb
@@ -14,6 +14,11 @@ RSpec.describe GameQuestion, type: :model do
     it 'correct .answer_correct?' do
       expect(game_question.answer_correct?('b')).to be_truthy
     end
+
+    it 'correct .level & .text delegates' do
+      expect(game_question.text).to eq(game_question.question.text)
+      expect(game_question.level).to eq(game_question.question.level)
+    end
   end
 
   context 'user helpers' do
